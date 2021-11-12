304aff6be007d9eb164019625681a143acd6006c
ipakey <bornagainyfke@gmai.com>
2021-11-12 12:10:59 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Gallery</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="gallery">
+        <a href="facilities/books-big.jpg"> <img src="facilities/books-small.jpg"  alt="bookshelves" ></a>
+        <a href="facilities/puzzlesandgames-big.jpg"> <img src="facilities/puzzlesandgames-small.jpg"  alt="puzzles" ></a>
+        <a href="facilities/cooking-big.jpg"> <img src="facilities/cooking-small.jpg"  alt="cooking" ></a>
+        <a href="facilities/handwashing-big.jpg"> <img src="facilities/handwashing-small.jpg"  alt="handwashing" ></a>
+        <a href="facilities/outside2-big.jpg"> <img src="facilities/outside2-small.jpg"  alt="outside" ></a>
+        <a href="facilities/gate security-big.jpg"> <img src="facilities/gate security-small.jpg"  alt="gate lock" ></a>
+        <a href="facilities/workingtable-big.jpg"> <img src="facilities/workingtable-small.jpg"  alt="work table" ></a>
+
+    </div>
+</body>
+</html>
