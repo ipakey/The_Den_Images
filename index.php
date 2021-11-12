a0e09bc55f3b86bd5e7a359f13baad4c63854d2a
ipakey <bornagainyfke@gmai.com>
2021-11-12 12:26:33 +0000

    install lightbox

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Gallery</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" />
+    <link href="lightbox.css" rel="stylesheet" />
 </head>
 <body>
     <div class="gallery">
@@ -18,5 +19,7 @@
         <a href="facilities/workingtable-big.jpg"> <img src="facilities/workingtable-small.jpg"  alt="work table" ></a>
 
     </div>
+
+<script src="lightbox.js"></script>
 </body>
 </html>
